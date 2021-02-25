c66f6bd02c39f06eda6958ea5d5d25a4ce564f2b
karamuz <meletis.aek21@gmail.com>
2021-02-25 09:37:04 +0200

    test

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return ( 
+        <nav className="Navbar">
+            <a href="/">home</a>
+            <a href="/payments">you got to pay</a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
